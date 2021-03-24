8a52eec628fbabf4c2fdb17d4e843e6e6d4227bc
Medhy Dohou <medhy.dohou@etu.univ-grenoble-alpes.fr>
2021-03-24 10:50:35 +0100

    Ajout verification des exercices et insertion BDD

--- src/Controller/ExerciseSubmissionController.php
+++ src/Controller/ExerciseSubmissionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Exercise;
+use App\Entity\ExerciseState;
+use App\Entity\Language;
+use App\Entity\Restricted;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,17 +37,39 @@ class ExerciseSubmissionController extends AbstractController
         $form->handleRequest($req);
         if($form->isSubmitted()){
             $user = $this->getUser();
-            $newFilename = $user->getId() . "-" . date('d.m.Y') . '-' . date('H:i') . "-" . uniqid() . ".zip";
+            $newFilename = $user->getId() . "-" . date('d-m-Y') . '-' . date('H:i') . "-" . uniqid() . ".zip";
             $file = $form->getData()["fichier"];
+            $exerciseName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);;
             $file->move('/tmp',$newFilename);
-            $isArchiveValid = $this->verifyExerciseArchive('/tmp/'.$newFilename);
+            $archivePath = '/tmp/'.$newFilename;
+            $isArchiveValid = $this->verifyExerciseArchive($archivePath);
             if(!$isArchiveValid){
+                unlink($archivePath);
                 return $this->render('exercise_submission/index.html.twig',[
                     'form' => $form->createView(),
                     'error' => 'Le format de l\'archive est incorrect.'
                 ]);
             }
-            parseArchive('/tmp/'.$newFilename);
+            $tempFolderPath = explode('.',$archivePath)[0];
+            if(is_dir(Exercise::$PATH_TO_EXERCISES_FOLDER . '/' . $exerciseName)){ // Vérifie si le dossier avec un même nom existe déjà
+                return $this->render('exercise_submission/index.html.twig',[
+                    'form' => $form->createView(),
+                    'error' => 'Il y\'a déjà un exercice avec ce nom'
+                ]);
+            }
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $exerciseAndRestrictions = $this->parseExercise($tempFolderPath,$exerciseName);
+            $entityManager->persist($exerciseAndRestrictions[0]);
+            $entityManager->flush();
+            $exerciseAndRestrictions[1]->setExerciseId($exerciseAndRestrictions[0]);
+            $entityManager->persist($exerciseAndRestrictions[1]);
+            $entityManager->flush();
+            unlink($archivePath);
+            return $this->render('exercise_submission/index.html.twig',[
+                'form' => $form->createView(),
+                'success' => "Exercice soumis avec succés ! Veuillez attendre la validation d'un administrateur."
+            ]);
         }
         return $this->render('exercise_submission/index.html.twig',[
             'form' => $form->createView(),
@@ -63,18 +89,27 @@ class ExerciseSubmissionController extends AbstractController
         if($zip->open($pathToArchive)){
             for($i = 0; $i < $zip->numFiles; $i++)
             {  
-                $numberOfMatches = 0;
-                $filename = $zip->getNameIndex($i);
-                foreach($filesNamesRegex as $regex){
-                    $numberOfMatches += preg_match($regex,$filename);
+                $stat = $zip->statIndex($i);
+                if($stat["size"] != 0){ // Permet de s'assurer que le fichier n'est pas un dossier.
+                    $numberOfMatches = 0;
+                    $filename = $zip->getNameIndex($i);
+                    foreach($filesNamesRegex as $regex){
+                        $numberOfMatches += preg_match($regex,$filename);
+                    }
+                    if($numberOfMatches == 0){
+                            return false; // Si le nom de fichier ne corresponds à aucune REGEX, alors ce fichier ne devrait pas être dans l'archive, 
+                                          // et la validité de l'archive est compromise
+                    }
                 }
-                if($numberOfMatches == 0){
-                        return false; // Si le nom de fichier ne corresponds à aucune REGEX, alors ce fichier ne devrait pas être dans l'archive, 
-                                      // et la validité de l'archive est compromise
+                else{
+                    return false; // Si c'est un dossier, rejeter l'archive. (La spécification du format d'archive ne prends
+                                  // aucun dossier.
                 }
+                
             } 
         }
-        return $this->checkSettings($pathToArchive);
+        $pathToFolder = explode('.',$pathToArchive)[0];
+        return $this->checkSettings($pathToArchive,$pathToFolder);
     }
 
     /**
@@ -83,32 +118,65 @@ class ExerciseSubmissionController extends AbstractController
      * @param String $pathToArchive The path to the archive to check integrity of.
      * @return boolean true if the settings are proper, false otherwise
      */
-    public function checkSettings(String $pathToArchive): bool
+    public function checkSettings(String $pathToArchive, String $pathToFolder): bool
     {
         $zip = new ZipArchive;
-        $tmpFolderPath = '/tmp/' . explode('.',$pathToArchive)[0];
         if($zip->open($pathToArchive)){
-            $zip->extractTo($tmpFolderPath, array("settings.json"));
-            $rawJsonText = file_get_contents($tmpFolderPath . "/settings.json");
+            $zip->extractTo($pathToFolder);
+            $rawJsonText = file_get_contents($pathToFolder . "/settings.json");
             $settingsArray = json_decode($rawJsonText,true);
             $isDifficultyValid = (gettype($settingsArray['difficulty']) === "integer") && ($settingsArray['difficulty'] >= 0) && 
             ($settingsArray['difficulty'] <= 10);
-            $isLanguageValid = gettype($settingsArray['language']) === "string";
+            $isLanguageValid = gettype($settingsArray['language']) === "array";
+            
             $isTimeoutValid = (gettype($settingsArray['timeout']) === "integer") && ($settingsArray['timeout']>=0);
             return $isDifficultyValid && $isLanguageValid && $isTimeoutValid;
-        }
-        else{
-            return false;
         }
         return true;
     }
     /**
-     * This method parse the archive and transforms it into an exercise object
+     * This method parse the archive and transforms it into an array containing an exercise and a restricted object.
+     * It also moves the directory from the temporary folder of the machine to the exercises folder.
      *
-     * @param String $pathToArchive The path to the archive to check integrity of.
-     * @return void
+     * @param String $pathToFolder The path to the exercise temp folder to parse.
+     * @param String $exerciseName The name of the exercise. Used for the exercise folder.
+     * @return Array The generated array of exercise and constraint.
      */
-    public function parseArchive(String $pathToFolder){
+    public function parseExercise(String $pathToTempFolder, String $exerciseName): Array
+    {
+        $exercise = new Exercise();
+        $languageRestrictions = new Restricted();
+        $usersRepo = $this->getDoctrine()->getRepository(User::class);
+        $exerciseStateRepo = $this->getDoctrine()->getRepository(ExerciseState::class);
+        $languageRepo = $this->getDoctrine()->getRepository(Language::class);
+        $creator = $usersRepo->find($this->getUser()->getId());
+        
+        $exerciseState = $exerciseStateRepo->find(1);
+        $settingsContent = file_get_contents($pathToTempFolder . '/settings.json');
+        $settingsArray = json_decode($settingsContent,true);
+        $exercise->setCreator($creator);
+        $exercise->setDescription(file_get_contents($pathToTempFolder . '/description.txt'));
+        $exercise->setDifficulty($settingsArray["difficulty"]);
+        $exercise->setFolderPath(Exercise::$PATH_TO_EXERCISES_FOLDER . "/" . $exerciseName);
+        $exercise->setState($exerciseState);
+        $exercise->setName($exerciseName);
+        $exercise->setSubmitDate(new \DateTime());
 
+        // Déplacement du dossier temporaire
+        mkdir(Exercise::$PATH_TO_EXERCISES_FOLDER . '/' . $exerciseName);
+        $fichiers = array_diff(scandir($pathToTempFolder), Array('.','..'));
+        foreach($fichiers as $fichier){
+            rename($pathToTempFolder . '/' . $fichier, 
+                    Exercise::$PATH_TO_EXERCISES_FOLDER . '/' . $exerciseName . '/' . $fichier);
+        }
+        
+        
+        foreach($settingsArray["language"] as $languageConstraint){
+            $language = $languageRepo->findOneBy([
+                'name' => $languageConstraint,
+            ]);
+            $languageRestrictions->addLanguage($language);
+        }
+        return Array($exercise,$languageRestrictions);
     }
 }

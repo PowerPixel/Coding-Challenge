a5ca50f5c44699fd6295993baac9f61df135c48f
Émilie Vey <nathan.vey@etu.univ-grenoble-alpes.fr>
2021-03-31 09:19:09 +0200

    ksorting is broken maybe? Yes it is.

--- src/Controller/ExerciseController.php
+++ src/Controller/ExerciseController.php
@@ -31,7 +31,7 @@ class ExerciseController extends AbstractController
                 $users[$username] = $score;
             }
         }
-        asort($users);
+        ksort($users);
         return $this->render('exercise/index.html.twig', [
             "exercise" => $exercise,
             "user_scores" => $users,

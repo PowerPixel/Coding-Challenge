0676e78abfa046bb82c78478e9e440eaaf1baea5
Medhy Dohou <medhy.dohou@etu.univ-grenoble-alpes.fr>
2021-04-01 20:45:46 +0200

    Mise à jour du script d'installation

--- install.sh
+++ install.sh
@@ -11,12 +11,10 @@ echo "WARNING ! Before using this script, you must have installed VirtualBox, as
  To do so, please refer to the wiki of the project";
 echo "Please input the path where you'd like to download the camisole image (needs at least 1.5GiB of free space, absolute path)";
 read CAMISOLE_DL_PATH;
-downloadCamisole $CAMISOLE_DL_PATH;
+#downloadCamisole $CAMISOLE_DL_PATH;
 echo "Installing camisole through VBoxManage...";
 /usr/bin/VBoxManage import "$CAMISOLE_DL_PATH/camisole.ova";
 echo "Installing Docker image"
 docker build --tag coding-challenge ./docker;
 echo "Everything done !";
-printf "You can now launch the camisole VM through the command : \n";
-printf "\tvboxmanage startvm camisole-2019-01-23 --type headless";
 echo "You can launch the docker machine through the script called startDocker.sh, and camisole through the script startCamisole.sh";
